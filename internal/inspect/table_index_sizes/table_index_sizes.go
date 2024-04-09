4a3e67ca84c0f94fa5a3f0309e4b944143cfc9ac
Chris Gwilliams <517923+encima@users.noreply.github.com>
2024-04-09 13:33:57 +0300

    move queries to utils file in inspect module and refined to remove all supabase managed schemas

--- internal/inspect/table_index_sizes/table_index_sizes.go
+++ internal/inspect/table_index_sizes/table_index_sizes.go
@@ -18,7 +18,7 @@ SELECT c.relname AS table,
   pg_size_pretty(pg_indexes_size(c.oid)) AS index_size
 FROM pg_class c
 LEFT JOIN pg_namespace n ON (n.oid = c.relnamespace)
-WHERE n.nspname NOT IN ('pg_catalog', 'information_schema')
+WHERE n.nspname NOT IN ('pg_catalog', 'information_schema', 'auth', 'realtime', 'pgsodium', 'supabase_migrations', 'vault', 'storage')
 AND n.nspname !~ '^pg_toast'
 AND c.relkind='r'
 ORDER BY pg_indexes_size(c.oid) DESC;`
